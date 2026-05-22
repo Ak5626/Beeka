86aa694d9539370749b68fdf419f093883acacde
abishekb <abi46328@gmail.com>
2026-05-22 19:28:15 +0530

    git commit -m "feat: created homepage file for validation and assertion class"

---
+++ src/main/java/pages/homepage.java
@@ -0,0 +1,98 @@
+package pages;
+
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.Select;
+import utils.utility;
+
+
+import static pages.DriverFactory.driver;
+
+
+public class homepage {
+
+    public static String langConfirmation = null;
+    public boolean logoVal;
+    private static final By acceptCookies = By.xpath("//button[contains(text(),'Cookies accepteren')]");
+    private static final By logo = By.xpath("//img[@class='logo-desktop']");
+    // private static final By langOptions = By.xpath("//img[@class='logo-desktop']");
+    private static final By eng = By.xpath("//a[@lang='en' and @data-value='en']");
+    private static final By dutch = By.xpath("//a[@lang='nl' and @data-value='nl']");
+    private static final By rejectPopUp = By.xpath("//button[@type='button' and @class='modal__close | js-modal-close']");
+    public static final By engLang = By.xpath("//a[text()='Find a store']");
+    public static final By dutchLang = By.xpath("//a[text()='Verkooppunten']");
+    private static final By footerLogo = By.xpath("//footer[@class='page-footer']/div/div/div/div/a");
+    public static final By headerLangOpt = By.xpath("//li[@class='top-nav__list-item top-nav__localisation']/form");
+    public static final By changeOptToDutch = By.xpath("//ul[@id='lang-list']/li/a[@data-value='nl']");
+    public static final By changeOptToEng = By.xpath("//ul[@id='lang-list']/li/a[@data-value='en']");
+
+
+    public void validateHomepage() {
+        utility.waitCond(driver, driver.findElement(acceptCookies));
+        driver.findElement(acceptCookies).click();
+        utility.waitCond(driver, driver.findElement(rejectPopUp));
+        driver.findElement(rejectPopUp).click();
+        try {
+            if (utility.ElePresent(driver.findElement(engLang))) {
+                System.out.println("Homepage is loaded with Language \"English\"");
+                langConfirmation = "eng";
+            }
+        } catch (Exception e) {
+            try {
+                if (utility.ElePresent(driver.findElement(dutchLang))) {
+                    System.out.println("Homepage is loaded with Language \"Dutch\"");
+                    langConfirmation = "dutch";
+
+                }
+            } catch (Exception t) {
+                System.out.println("Homepage is loaded with different language");
+            }
+        }
+    }
+
+
+    public boolean validateLogoHeader() {
+        logoVal = utility.ElePresent(driver.findElement(logo));
+        return logoVal;
+    }
+
+    public boolean validateSwitchLanguage() {
+        utility.waitCond(driver, driver.findElement(headerLangOpt));
+        driver.findElement(headerLangOpt).click();
+        if (langConfirmation.equalsIgnoreCase("eng")) {
+            try {
+                driver.findElement(changeOptToDutch).click();
+                utility.waitCond(driver, driver.findElement(dutchLang));
+                boolean lang = utility.ElePresent(driver.findElement(dutchLang));
+                System.out.println("Switched to Dutch Language");
+                return lang;
+            } catch (Exception e) {
+                return false;
+            }
+
+        } else if (langConfirmation.equalsIgnoreCase("dutch")) {
+            try {
+                driver.findElement(changeOptToEng).click();
+                utility.waitCond(driver, driver.findElement(engLang));
+                boolean lang = utility.ElePresent(driver.findElement(engLang));
+                System.out.println("Switched to English Language");
+                return lang;
+            } catch (Exception e) {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+    }
+
+    public boolean validateLogoFooter() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView();", driver.findElement(footerLogo));
+        logoVal = utility.ElePresent(driver.findElement(footerLogo));
+        return logoVal;
+    }
+
+
+}

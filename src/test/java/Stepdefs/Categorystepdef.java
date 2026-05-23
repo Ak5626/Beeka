9e2fbff2de1a2e05512667b48515fb1649a6bbef
abishekb <abi46328@gmail.com>
2026-05-23 15:03:16 +0530

    git commit -m "feat: Added 2nd scenario in category feature and respective files"

--- src/test/java/Stepdefs/Categorystepdef.java
+++ src/test/java/Stepdefs/Categorystepdef.java
@@ -12,23 +12,28 @@ public class Categorystepdef {
     Categorypage cp = new Categorypage();
 
     @When("Homepage gets loaded click on main category {string}")
-        public void Homepagegetsloadedclickonmaincategory(String mainCat){
-            cp.chooseMainCat(mainCat);
+    public void Homepagegetsloadedclickonmaincategory(String mainCat) {
+        cp.chooseMainCat(mainCat);
     }
 
     @And("Choose any one product set {string}")
     public void chooseAnyOneProductSet(String product) {
-            cp.chooseProduct(product);
+        cp.chooseProduct(product);
     }
 
     @Then("Choose one category {string} and validate count between choosen one and product grid with index {string}")
-    public void chooseSelectOneCategoryAndValidateCountBetweenChoosenOneAndProductGrid(String category,String index) {
-           cp.chooseCategory(category);
-           String totalCountInProduct =  cp.checkCountInCategory(category,index);
-           String totalCountInGrid = cp.checkCountInGrid();
-           Assertions.booleanCheck(totalCountInGrid.contains(totalCountInProduct),"Count between selected category and count in product grid is not matched");
+    public void chooseSelectOneCategoryAndValidateCountBetweenChoosenOneAndProductGrid(String category, String index) {
+        cp.chooseCategory(category);
+        String totalCountInProduct = cp.checkCountInCategory(category, index);
+        String totalCountInGrid = cp.checkCountInGrid();
+        Assertions.booleanCheck(totalCountInGrid.contains(totalCountInProduct), "Count between selected category and count in product grid is not matched");
 
     }
 
 
+    @Then("Remove filtered category and validate checkbox is unchecked {string}")
+    public void removeFilteredCategoryAndValidateCheckboxIsUnchecked(String category) {
+        cp.chooseCategory(category);
+        Assertions.booleanCheck(cp.checkSelectedFilters(category), "Category is not still checked even filtered category removed");
+    }
 }
--- src/test/resources/features/Category.feature
+++ src/test/resources/features/Category.feature
@@ -11,3 +11,14 @@ Feature: Category Page validation
       |maincategory  |product|subCategory|indexForChoosingCount|
       |Cookware|Frying pans|Frying pans|0                      |
 
+  @Validate_CategoryPage_Scenario2
+  Scenario Outline: Choose category and unselect category
+
+    Given Navigate to homepage
+    When  Homepage gets loaded click on main category "<maincategory>"
+    And   Choose any one product set "<product>"
+    Then  Remove filtered category and validate checkbox is unchecked "<subCategory>"
+    Examples:
+      |maincategory  |product|subCategory|indexForChoosingCount|
+      |Cookware|Frying pans|Frying pans|0                      |
+

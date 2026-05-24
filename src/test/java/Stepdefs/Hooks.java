d06dcbab3bf092a4bec8caf90a8d02ddda79fbde
abishekb <abi46328@gmail.com>
2026-05-24 16:58:26 +0530

    git commit -m "feat: Created hooks class for multiple scenario execution "

---
+++ src/test/java/Stepdefs/Hooks.java
@@ -0,0 +1,36 @@
+package Stepdefs;
+
+import Utils.Assertions;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import utils.DriverFactory;
+import utils.utility;
+
+import java.time.Duration;
+
+import static utils.DriverFactory.driver;
+
+public class Hooks {
+
+    @Before
+    public void setup(Scenario scenario) throws Exception {
+        System.out.println("Starting: " + scenario.getName());
+        DriverFactory.initDriver("chrome");
+        if(utility.getValue("env").equalsIgnoreCase("prod")) {
+            driver.get(utility.getValue("eng_url"));
+            driver.manage().window().maximize();
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(15));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        }else{
+            System.out.println("No environment found");
+        }
+
+    }
+
+    @After
+    public void tearDown(){
+        Assertions.assertAll();
+        driver.quit();
+    }
+}

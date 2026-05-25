63030c0ba4a11a9e95618eba6ca1aabf38e6d2e4
abishekb <abi46328@gmail.com>
2026-05-25 10:47:13 +0530

    git commit -m "refactor: Updated test runner for mvn execution"

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,30 +1,14 @@
 package Runner;
 
-import Utils.Assertions;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
-import io.cucumber.java.Scenario;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeTest;
-import utils.DriverFactory;
-import utils.utility;
-
-import java.time.Duration;
-
-import static utils.DriverFactory.driver;
-
 
 
 @CucumberOptions(
             features = "src/test/resources/features",
             glue = "Stepdefs",
-            plugin = {"pretty", "html:target/cucumber-report.html"},
-
-            tags= "@Validate_Searchfield_Scenario1")
+            plugin = {"pretty", "html:target/cucumber-report.html"}
+)
 
 public class TestRunner extends AbstractTestNGCucumberTests {
 
